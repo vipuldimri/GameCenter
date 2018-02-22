8482a72648a4a7c21f0f87d4e3c9081c9ba6a5d6
vipul <dimrivipul.vd@gmail.com>
2018-02-22 19:22:19 +0530

    Database Connection MYSQL

--- src/Database/UserImplements.java
+++ src/Database/UserImplements.java
@@ -1,15 +1,18 @@
 
 package Database;
 
+import gamecenter.Stall;
 import gamecenter.User;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class UserImplements implements UserInterface
 {
 
-    static final String GetAllUser = "SELECT * FROM";
-    
+    static final String GetAllUser = "SELECT * FROM gamecenter.users";
+    static final String GetAllStalls = "SELECT * FROM gamecenter.gamezone;";
     Connection conn;
     UserImplements()
     {
@@ -20,7 +23,49 @@ public class UserImplements implements UserInterface
     @Override
     public ArrayList<User> getAllUsers() 
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<User> users = new ArrayList<>();
+         try {
+                   Statement stmt=conn.createStatement();  
+                   ResultSet rs = stmt.executeQuery(GetAllUser);
+                   while(rs.next())  
+                   {
+                    User newuser = new User();
+                    users.add(newuser);
+                   }
+        
+        } catch (Exception ex) 
+        {
+                System.out.println("AAA "+ex);
+        }
+        return users;
+    }
+
+    @Override
+    public ArrayList<Stall> getAllStall()
+    {
+      
+     ArrayList<Stall> stalls = new ArrayList<>();
+         try {
+                   Statement stmt=conn.createStatement();  
+                   ResultSet rs = stmt.executeQuery(GetAllStalls);
+                   while(rs.next())  
+                   {
+                    Stall newstall = new Stall();
+                    stalls.add(newstall);
+                   }
+        
+        } catch (Exception ex) 
+        {
+                System.out.println("AAA "+ex);
+        }
+        try {
+            conn.close();
+        } catch (Exception ex) 
+        {
+                System.out.println("BBB "+ex);
+     
+        }
+        return stalls;
     }
     
 }
