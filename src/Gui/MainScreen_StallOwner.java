86c0d38dc24d558894d5b86903ae9a0775bcccf8
u20o90 <u20o90@INGURWN120006.avl01.avlcorp.lan>
2018-04-13 20:25:42 +0530

    trasaction filter inclusive added

--- src/Gui/MainScreen_StallOwner.java
+++ src/Gui/MainScreen_StallOwner.java
@@ -1948,8 +1948,19 @@ public class MainScreen_StallOwner extends javax.swing.JFrame
         }
         try
         {
-            
-         startdate  = jDateChooser_StartDate.getDate(); 
+            startdate  = jDateChooser_StartDate.getDate(); 
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(startdate);
+ 
+// Set time fields to zero
+cal.set(Calendar.HOUR_OF_DAY, 0);
+cal.set(Calendar.MINUTE, 0);
+cal.set(Calendar.SECOND, 0);
+cal.set(Calendar.MILLISECOND, 0);
+ 
+// Put it back in the Date object
+         startdate = cal.getTime();
+         //startdate  = jDateChooser_StartDate.getDate(); 
         
          enddate =jDateChooser_EndDate.getDate();
         }
